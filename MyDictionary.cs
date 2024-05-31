89ce76ceec6ad9318a96a11dccf464b6d5ea636c
onurknblt <onurknblt@gmail.com>
2024-05-31 16:23:35 +0300

    Dictionary Projesi
    Anahtar ve değer kavramlarını anlamlandırabildiğim Dictionary class projem

---
+++ MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖdevMyList
+{
+    internal class MyDictionary<TKey,TValue>
+    {
+        public TKey[] keys;
+        public TValue[] values;
+        public int Length = 0;
+
+        public MyDictionary() 
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+            Length = 0;
+        }
+
+        public void Add(TKey key, TValue value)      
+        { 
+            TKey[] tempKeys = new TKey[keys.Length+1];
+            TValue[] tempValues = new TValue[keys.Length+1];
+
+            for (int i = 0; i < tempKeys.Length; i++) 
+            {
+                tempKeys[i] = keys[i];
+                tempValues[i] = values[i];           
+            } 
+        
+        
+        
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ÖdevMyList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyDictionary <int,string> myDictionary = new MyDictionary <int,string>();
+
+            //Veri ekleme
+            myDictionary.Add(38, "Kayseri");
+            myDictionary.Add(60, "Tokat");
+            myDictionary.Add(35, "İzmir");
+
+            // Eklenen verileri ekrana yazdırma
+            for (int i = 0; i < myDictionary.Length;i++)
+            {
+                Console.WriteLine("Anahtar: " + myDictionary.keys[i] + ", Değer: " + myDictionary.values[i]);
+            }
+        }
+    }
+}
